f3b9926891be13c35327699008e7b03ad93f1971
FelipeGSG <160530177+FelipeGSG@users.noreply.github.com>
2025-08-24 20:14:00 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ const upload = multer({ storage: storage });
 
 // Middlewares padrão
 app.use(express.static('public'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
 app.use(express.json());
 
 // Rotas próprias
