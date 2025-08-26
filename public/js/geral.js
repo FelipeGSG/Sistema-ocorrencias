68fa234343c2c61cb14d2368821f7fe9a9c238c8
FelipeGSG <160530177+FelipeGSG@users.noreply.github.com>
2025-08-26 14:33:29 -0300

    Update geral.js

--- public/js/geral.js
+++ public/js/geral.js
@@ -1,4 +1,5 @@
 const porta = 3000
-const link = `http://localhost:3000`
+const link = `https://sistema-ocorrencias.onrender.com`
 
-// https://sistema-ocorrencias.onrender.com
+
+// http://localhost:3000
